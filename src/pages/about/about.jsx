731a4bb8917487df5784367a950096ddb1af3abe
Baptiste Boucly <b.boucly@outlook.com>
2024-04-03 20:49:14 +0200

    adding img and about on index.js

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,9 +1,18 @@
-
-
+import img from '../../assets/about.webp'
+import Banner from '../../components/banner/banner'
 import './about.css'
 
 const About = () => {
   
+  return (
+    <main id="about">
+
+      <Banner
+        src={img}
+        alt="Logo Kasa"
+        />
+    </main>
+  )
 }
 
 export default About
