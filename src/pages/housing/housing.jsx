e6bffb958cda94292e986c600eeddaa0efef3a11
Baptiste Boucly <b.boucly@outlook.com>
2024-04-25 19:53:00 +0200

    finishing housing jsx

--- src/pages/housing/housing.jsx
+++ src/pages/housing/housing.jsx
@@ -4,6 +4,9 @@ import data from "../../assets/data.json";
 import Error from "../error/error";
 
 import Carousel from "../../components/carousel/carousel";
+import Collapse from "../../components/collapse/collapse";
+import Host from "../../components/host/host";
+import Rating from "../../components/rating/rating";
 import "./housing.css";
 
 
@@ -30,10 +33,36 @@ const Housing = () => {
           <p>{card.location}</p>
 
           <ul>
-        
+            {card.tags.map((tag) =>
+
+              <li key={tag}>
+                <label>{tag}</label>
+              </li>
+
+            )}
           </ul>
         </header>
+        <figure>
+          <Rating rating={card.rating} />
+
+          <Host
+            name={card.host.name}
+            picture={card.host.picture}
+          />
+        </figure>
       </section>
+      
+      <footer>
+        <Collapse
+          title={data.text}
+          content={card.description}
+        />
+
+        <Collapse
+          title={data.list}
+          content={card.equipments}
+        />
+      </footer>
     </main>
   );
 }
