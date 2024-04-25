4e1c8715713e83b51c2bdc1c58cc1d8296d2c9bf
Baptiste Boucly <b.boucly@outlook.com>
2024-04-25 19:55:18 +0200

    host and rating main structure

---
+++ src/components/host/host.jsx
@@ -0,0 +1,25 @@
+import "./host.css";
+
+const Host = ({ name, picture }) => {
+
+  const [firstname, lastname] = name.split(" ");
+
+  return (
+    <figure className="host">
+
+      <figcaption>
+        {firstname}
+        <br />
+        {lastname}
+      </figcaption>
+
+      <img
+        src={picture}
+        alt={name}
+      />
+
+    </figure>
+  )
+}
+
+export default Host
